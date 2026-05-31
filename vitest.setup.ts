7dc6cefccc82a3ece9fe59de1250b1252d3b0c0e
briankane <bkane56@gmail.com>
2026-05-31 10:37:02 -0400

    fix the jest-axe matcher import

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -2,8 +2,4 @@ import '@testing-library/jest-dom/vitest';
 import { expect } from 'vitest';
 import { toHaveNoViolations } from 'jest-axe';
 
-const jestAxeMatchers: Parameters<typeof expect.extend>[0] = {
-  toHaveNoViolations,
-};
-
-expect.extend(jestAxeMatchers);
+expect.extend(toHaveNoViolations);
